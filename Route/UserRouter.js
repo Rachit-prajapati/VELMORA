1726122d68011bbe4259fcefc15a32967425e192
Rachit Prajapati <prajapatirachit75@gmail.com>
2026-04-14 01:53:55 +0530

    Some Basic Change For ui Related

--- Route/UserRouter.js
+++ Route/UserRouter.js
@@ -7,7 +7,7 @@ UserRouter.get("/",verifyPublic,getRecord);
 UserRouter.get("/:_id",verifyPublic,getSingleRecord);
 UserRouter.post("/",verifyPublic,createRecord);
 UserRouter.post("/login",verifyPublic,loginIn);
-UserRouter.post("/forget-password-1",verifyPublic,forgetPassword1);
+UserRouter.post("/forget-password-1",forgetPassword1);
 UserRouter.post("/forget-password-2",verifyPublic,forgetPassword2);
 UserRouter.post("/forget-password-3",verifyPublic,forgetPassword3);
 UserRouter.put("/:_id",verifyBuyer,updateRecord);
